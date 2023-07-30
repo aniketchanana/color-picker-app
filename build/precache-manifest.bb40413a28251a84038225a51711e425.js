f0dc302af2b8462fce992a5e574fc86fc2848180
aniket <aniket.chanana@gmail.com>
2023-07-30 22:31:00 +0530

    Added code for deployment

---
+++ build/precache-manifest.bb40413a28251a84038225a51711e425.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c7186f1ec9f24e76c1365851332bf7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7647256d2ff267cd9850",
+    "url": "/static/css/2.292c8731.chunk.css"
+  },
+  {
+    "revision": "c9a28469db3338ff048f",
+    "url": "/static/css/main.cd78f3b3.chunk.css"
+  },
+  {
+    "revision": "7647256d2ff267cd9850",
+    "url": "/static/js/2.0899ffc8.chunk.js"
+  },
+  {
+    "revision": "c9a28469db3338ff048f",
+    "url": "/static/js/main.c793a43f.chunk.js"
+  },
+  {
+    "revision": "f97ad6521d96ed512b45",
+    "url": "/static/js/runtime-main.dcb84ad5.js"
+  },
+  {
+    "revision": "408cd92e709c2a344cbe258979fcdd3d",
+    "url": "/static/media/bg.408cd92e.svg"
+  }
+]);
